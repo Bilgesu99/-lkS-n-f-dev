48cd0c0d8551b8f7dea5c13369882cf62e051ab9
asus <asus@DESKTOP-OTTG4TB>
2024-08-18 17:55:05 +0300

    Add project files.

---
+++ İlkSınıfÖdev/Person.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace İlkSınıfÖdev
+{
+    public class Person
+    {
+        public string ad;
+        public string soyad;
+        public DateTime doğumTarihi;
+
+
+        public string Ad //Metotları yazıyoruz.
+        {
+            get
+            {
+                return ad;
+            }
+            set
+            {
+                ad=value;
+            }
+        }
+
+        public string Soyad
+        {
+            get
+            {
+                return soyad;
+            }
+            set
+            {
+                soyad=value;
+            }
+        }
+
+        public DateTime DoğumTarihi
+        {
+            get
+            {
+                return doğumTarihi;
+            }
+            set
+            {
+                doğumTarihi=value;
+            }
+        }
+        public void teacherInfo()
+        {
+            Console.WriteLine("Öğretmenin adı ve soyadı " + ad + " " + soyad + " "+ "doğum tarihi: " + doğumTarihi.ToShortDateString());
+        }
+
+        public void studentInfo()
+        {
+            Console.WriteLine("Öğrencinin adı ve soyadı " + ad + " " + soyad + " " + "doğum tarihi: " + doğumTarihi.ToShortDateString());
+
+
+
+
+        }
+
+        
+    }
+
+
+
+}
---
+++ İlkSınıfÖdev/Program.cs
@@ -0,0 +1,63 @@
+﻿
+
+using İlkSınıfÖdev;
+using System.ComponentModel;
+
+class program
+{
+    public static void Main(string[] args)
+    {
+        Person student1 = new Person();
+        student1.Ad = "Ahmet";
+        student1.Soyad = "Yıldırım";
+        student1.DoğumTarihi = new DateTime(2007, 3, 18);
+
+        student1.studentInfo();
+
+        Person student2 = new Person();
+        student2.Ad = "Mahmut";
+        student2.Soyad = "Akyüz";
+        student2.DoğumTarihi = new DateTime(2008, 3, 18);
+
+        student2.studentInfo();
+
+
+        Person teacher1= new Person();
+        teacher1.Ad = "Ayşe";
+        teacher1.Soyad = "Korkma";
+        teacher1.DoğumTarihi = new DateTime(1987, 5, 7);
+
+        teacher1.teacherInfo();
+
+        Person teacher2= new Person();
+        teacher2.Ad = "Mustafa";
+        teacher2.Soyad = "Hayal";
+        teacher2.DoğumTarihi = new DateTime(1978, 7, 8);
+
+        teacher2.teacherInfo();
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
